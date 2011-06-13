38115462006cf2637a17cedc737060fdf5530028
diceratops <diceratops@sadie.(none)>
2011-06-12 21:46:33 -0400

    random number game

---
+++ game.rb
@@ -0,0 +1,18 @@
+puts "Hello player, "
+puts "Please guess a number between 1 and 10"
+
+guess = gets.to_i
+
+num = rand(10)
+
+if guess == num
+  puts "good job"
+elsif guess > num
+  puts "that is too high"
+elsif guess < num
+  puts "that is too low"
+end
+
+puts "The number was " + num.to_s
+
+
