ddeb1c81376d6aa81c22e124598e9cbd65389601
diceratops <diceratops@sadie.(none)>
2011-06-13 22:06:25 -0400

    fixed numbers game by adding +1

--- game.rb
+++ game.rb
@@ -1,9 +1,11 @@
+# a simple random number generator guessing game
+
 puts "Hello player, "
 puts "Please guess a number between 1 and 10"
 
 guess = gets.to_i
 
-num = rand(10)
+num = rand(10) + 1
 
 if guess == num
   puts "good job"
