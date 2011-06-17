07e4c61c23b1fff89a29727268ef7cc25d811fae
diceratops <diceratops@sadie.(none)>
2011-06-16 21:04:10 -0400

    Converts a hash to an array

---
+++ ruby/hashToArrayLong.rb
@@ -0,0 +1,12 @@
+myHash = {:hat => "haberdasher", :shoe => "cobbler", :hair => "stylist"}
+
+myArray = []
+
+myHash.each {|item| myArray.push item} 
+
+
+#Or, there's the easy way
+
+#myHash.to_a
+
+
