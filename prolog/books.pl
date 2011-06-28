2dd1ffa4ce1f088471854796bf695b154a5b9986
diceratops <diceratops@sadie.(none)>
2011-06-28 15:36:02 -0400

    a library using prolog

---
+++ prolog/books.pl
@@ -0,0 +1,11 @@
+wrote(asimov, foundation).
+wrote(asimov, foundationAndEarth).
+wrote(asimov, foundationAndEmpire).
+wrote(asimov, secondFoundation).
+
+wrote(king, wizardAndGlass).
+wrote(king, songOfSusannah).
+wrote(king, wastelands).
+wrote(king, gunSlinger).
+
+written_by(X, Y) :- wrote(X, Y).
